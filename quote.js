2f10c23b7e00f7196e0203f30683d6e5385fa274
Elissa <elissadusabe@gmail.com>
2023-04-26 21:23:23 +0200

    Added functioning on button ad quotes are being changed

--- quote.js
+++ quote.js
@@ -1,6 +1,6 @@
-const btn = document.querySelector('#new-quote');
-const quote = document.querySelector('.quote');
-const person = document.querySelector('.person');
+let btn = document.querySelector('#new-quote');
+let quote = document.querySelector('.quote');
+let person = document.querySelector('.person');
 
 const quotes = [
     {
@@ -22,5 +22,12 @@ const quotes = [
     {
         quote: `"Believe you can and you're halfway there." `,
         person: ` Theodore Roosevelt`
-    }
+    },
 ]; 
+
+btn.addEventListener('click', function() {
+    let random = Math.floor(Math.random() * quotes.length);
+    quote.innerText = quotes[random].quote;
+    person.innerText = quotes[random].person;
+    console.log('btn clicked');
+})
