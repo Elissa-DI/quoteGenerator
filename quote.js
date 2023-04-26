92b31dab028bc9f5150508200ee4a1209f49d987
Elissa <elissadusabe@gmail.com>
2023-04-26 21:09:26 +0200

    Added quotes

---
+++ quote.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector('#new-quote');
+const quote = document.querySelector('.quote');
+const person = document.querySelector('.person');
+
+const quotes = [
+    {
+        quote: `"The best way to find yourself in the service of others"`,
+        person: `Mahtma Gandhi`
+    },
+    {
+        quote: `"The only true wisdom is in knowing you know nothing.`,
+        person: `Socrates`
+    },
+    {
+        quote: `Success is not final, failure is not fatal: it is the courage to continue that counts."`,
+        person: `Winston Churchill`
+    },
+    {
+        quote: `"We cannot solve our problems with the same thinking we used when we created them."`,
+        person: `Albert Einstein`
+    },
+    {
+        quote: `"Believe you can and you're halfway there." `,
+        person: ` Theodore Roosevelt`
+    }
+]; 
